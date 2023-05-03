c5003e63cf48fcad713cf83008157feb0a0c2ba6
yasminachavez <yasminachavez@yahoo.com>
2023-05-02 17:46:56 -0700

    added newItem function

--- index.js
+++ index.js
@@ -1,10 +1,10 @@
-function newImage(x, a, b) {
+function newImage(i, x, y) {
     let newImage = document.createElement('img')
-    newImage.src = (x)
-    newImage.style.position = 'fixed';
-    newImage.style.left = (a);
-    newImage.style.bottom = (b);
-    document.body.append(newImage);
+    newImage.src = (i)
+    newImage.style.position = 'fixed'
+    newImage.style.left = (x)
+    newImage.style.bottom = (y)
+    document.body.append(newImage)
 }
 newImage('assets/crate.png', '150px','200px')
 newImage('assets/well.png', '500px', '425px')
@@ -20,4 +20,18 @@ newImage('assets/pillar.png', '350px', '100px')
 // greenCharacter.style.bottom = '100px'
 // document.body.append(greenCharacter)
 
+function newItem (i, x, y) {
+let newItem = document.createElement('img')
+newItem.src = (i)
+newItem.position = 'fixed'
+newItem.left = (x)
+newItem.bottom = (y)
+document.body.append(newItem)
 
+newItem.addEventListener('click', function(){
+    newItem.remove()
+})
+}
+newItem('assets/sword.png', '500px', '405px')
+newItem('assets/shield.png', '165px', '185px')
+newItem('assets/staff.png', '600px', '100px')
