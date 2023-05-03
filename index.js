20522be1f5e90a7a4f227aec29254db947f8aa6f
yasminachavez <yasminachavez@yahoo.com>
2023-05-02 17:24:19 -0700

    newImage function complete

--- index.js
+++ index.js
@@ -1,24 +1,23 @@
-function newImage() {
+function newImage(x, a, b) {
     let newImage = document.createElement('img')
-    newImage.src = 'assets/img'
+    newImage.src = (x)
     newImage.style.position = 'fixed';
-    newImage.style.left = '300px';
-    newImage.style.bottom = '300px';
+    newImage.style.left = (a);
+    newImage.style.bottom = (b);
     document.body.append(newImage);
 }
+newImage('assets/crate.png', '150px','200px')
+newImage('assets/well.png', '500px', '425px')
+newImage('assets/green-character.gif', '100px', '100px')
+newImage('assets/pine-tree.png', '450px', '200px')
+newImage('assets/tree.png', '200px', '300px')
+newImage('assets/pillar.png', '350px', '100px')
 
-let greenCharacter = document.createElement('img')
-greenCharacter.src = 'assets/green-character.gif'
-greenCharacter.style.position = 'fixed'
-greenCharacter.style.left = '100px'
-greenCharacter.style.bottom = '100px'
-document.body.append(greenCharacter)
+// let greenCharacter = document.createElement('img')
+// greenCharacter.src = 'assets/green-character.gif'
+// greenCharacter.style.position = 'fixed'
+// greenCharacter.style.left = '100px'
+// greenCharacter.style.bottom = '100px'
+// document.body.append(greenCharacter)
 
-let pineTree = document.createElement('img')
-pineTree.src = 'assets/pine-tree.png'
-pineTree.style.position = 'fixed'
-pineTree.style.left = '450px'
-pineTree.style.bottom = '200px'
-document.body.append(pineTree)
 
-newImage()
